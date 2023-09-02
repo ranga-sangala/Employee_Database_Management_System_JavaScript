d06ce5a92e4e9241a5add21c3f2bfc55bf7704d5
ranga-sangala <rangasangala7@gmail.com>
2023-09-02 23:14:25 +0530

    Adding and Deleting employeee

--- script.js
+++ script.js
@@ -10,12 +10,67 @@
      const employeeList = document.querySelector(".employees__names--list");
      const employeeInfo = document.querySelector(".employees__names--info")
       
+
+     //add Employee Logic 
+     
+     const createEmployee = document.querySelector(".createEmployee");
+     const addEmployeeModal = document.querySelector(".addEmployee");
+     const addEmployeeForm = document.querySelector(".addEmployee_create");
+
+     createEmployee.addEventListener("click",()=>{
+        addEmployeeModal.style.display = "flex";
+     })
+
+
+     addEmployeeModal.addEventListener("click",(e)=>{
+            if(e.target.className === "addEmployee"){
+                addEmployeeModal.style.display = "none"
+            }
+     });
+
+
+     const dobInput = document.querySelector("addEmployee_create--dob");
+    
+
+     addEmployeeForm.addEventListener("submit",(e) =>{
+            e.preventDefault();
+            const formData = new FormData(addEmployeeForm)
+            const values = [...formData.entries()];
+            let empData = {}
+            values.forEach((val) =>{
+                empData[val[0]] = val[1]
+            });
+
+            empData.id = employees[employees.length - 1].id + 1;
+            empData.age = new Date().getFullYear() - parseInt(empData.dob.slice(0,4),10);
+            empData.imageUrl = empData.imageUrl || "https://cdn-icons-png.flaticon.com/512/0/93.png";
+            employees.push(empData);
+            renderEmployees();
+            addEmployeeForm.reset();
+            addEmployeeModal.style.display = "none"
+
+            
+        })
+
+
      employeeList.addEventListener('click',(e)=>{
            if(e.target.tagName === "SPAN" && selectedEmployeeId !== e.target.id){
                  selectedEmployeeId = e.target.id; 
                  renderEmployees();
                  renderSingleEmployee();
            }
+
+           if(e.target.tagName === "I"){
+            employees = employees.filter((emp) => String(emp.id) !== e.target.parentNode.id);
+           
+           if(String(selectedEmployeeId) === e.target.parentNode.id){
+            selectedEmployeeId = employees[0]?.id || -1;
+            selectedEmployee = employees[0] || {};
+            renderSingleEmployee();
+           }
+           
+        renderEmployees();
+        }
      });
 
 
@@ -33,7 +88,7 @@
             }
 
             employee.setAttribute("id",emp.id);
-            employee.innerHTML = `${emp.firstName}   ${emp.lastName} <p>X</P`
+            employee.innerHTML = `${emp.firstName}   ${emp.lastName} <i class="employeeDelete">❌</i>`
             employeeList.append(employee)
 
          });
@@ -41,6 +96,11 @@
 
 
        const renderSingleEmployee = () => {
+
+        if(selectedEmployeeId === -1){
+            employeeInfo.innerHTML ="";
+            return;
+        }
            employeeInfo.innerHTML = `
            <img src=" ${selectedEmployee.imageUrl}"/>
            <span class="employees__single--heading"> ${selectedEmployee.firstName} ${selectedEmployee.lastName}</span>
